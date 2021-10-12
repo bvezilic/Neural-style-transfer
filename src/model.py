e58292d82b4cd6909ceb7f6133ea1ee513771444
Branislav Vezilic <banex93@gmail.com>
2021-10-12 19:58:07 +0200

    Added documentation for model.py

--- src/model.py
+++ src/model.py
@@ -5,60 +5,80 @@ import torch.nn as nn
 import torchvision.models as models
 from torch import Tensor
 
+from src.utils import clone_tensors
+
 
 class VGG19(nn.Module):
-    def __init__(self,
-                 content_layers: List[int],
-                 style_layers: List[int]):
+    """
+    Pretrained VGG19. Uses hooks to extract outputs from different layers.
+
+    Args:
+        content_layers_ids (list): List of layer ids from which to extract output
+        style_layers_ids (list): List of layer ids from which to extract output
+    """
+
+    def __init__(
+            self,
+            content_layers_ids: List[int],
+            style_layers_ids: List[int]
+    ):
         super(VGG19, self).__init__()
 
         self.pretrained_vgg19 = models.vgg19(pretrained=True).features
 
-        self.content_layers = content_layers
-        self.style_layers = style_layers
+        self.content_layers_ids = content_layers_ids
+        self.style_layers_ids = style_layers_ids
 
-        self.content_features = OrderedDict({})
-        self.style_features = OrderedDict({})
+        # Cache attributes for temporary storing conv layers output (tensor)
+        self._content_features = OrderedDict({})
+        self._style_features = OrderedDict({})
 
-        # Register hooks to obtain various inputs
+        # Register hooks to obtain various outputs
         for idx, module in enumerate(self.pretrained_vgg19.children()):
-            if idx in self.content_layers:
+            if idx in self.content_layers_ids:
                 module.register_forward_hook(self._get_content_activation(idx))
 
-            if idx in self.style_layers:
+            if idx in self.style_layers_ids:
                 module.register_forward_hook(self._get_style_activation(idx))
 
     def _get_content_activation(self, idx: int) -> Callable:
+        """
+        Stores i-th layer activation to `self._content_features` dictionary.
+
+        Args:
+            idx (int): VGG19 layer index
+
+        Returns:
+            Callable: PyTorch hook function
+        """
+
         def hook(module, input, output) -> None:
-            self.content_features[idx] = output
+            self._content_features[idx] = output
 
         return hook
 
     def _get_style_activation(self, idx: int) -> Callable:
+        """
+        Stores i-th layer activation to `self._style_features` dictionary.
+
+        Args:
+            idx (int): VGG19 layer index
+
+        Returns:
+            Callable: PyTorch hook function
+        """
+
         def hook(module, input, output) -> None:
-            self.style_features[idx] = output
+            self._style_features[idx] = output
 
         return hook
 
     def clear_features(self) -> None:
         """
-        Clears stored features (outputs of conv layers) from hooks.
+        Clears stored features (outputs of conv layers).
         """
-        self.content_features = OrderedDict({})
-        self.style_features = OrderedDict({})
-
-    @staticmethod
-    def clone_features(features: Dict[int, Tensor]) -> Dict[int, Tensor]:
-        """
-        Clones all tensors in dictionary.
-
-        Args:
-            features (dict): Dictionary of tensors with string keys.
-
-        Returns:
-            New dictionary with cloned tensors.
-        """
-        return {idx: feature.clone() for idx, feature in features.items()}
+        self._content_features = OrderedDict({})
+        self._style_features = OrderedDict({})
 
     def forward(self, input_image: Tensor) -> (Tensor, Dict[int, Tensor], Dict[int, Tensor]):
         """
@@ -70,14 +90,14 @@ class VGG19(nn.Module):
         Returns:
             Outputs of conv layers picked up by hooks.
 
-            x (Tensor):
-            content_features (dict):
-            style_features (dict):
+            x (Tensor): Output of last feature layer from vgg19. Tensor of shape (B, 512, 8, 8)
+            content_features (dict): Outputs of conv layers at indices (keys of dicts)
+            style_features (dict): Outputs of conv layers at indices (keys of dicts)
         """
         x = self.pretrained_vgg19(input_image)
 
-        content_features = self.clone_features(self.content_features)
-        style_features = self.clone_features(self.style_features)
+        content_features = clone_tensors(self._content_features)
+        style_features = clone_tensors(self._style_features)
 
         self.clear_features()
 
