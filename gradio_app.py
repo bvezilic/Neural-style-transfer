d76bdcb68ed427221c31e1d2e94c65d06bae73e6
bvezilic <branislav.vezilic@gmail.com>
2023-07-16 21:39:29 +0200

    Changed description on gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -43,7 +43,7 @@ examples = [
 demo = gr.Interface(
     fn=style_transfer_fn,
     description="""
-    # Demo app for NST (Neural Style Transfer). Select one of the example below or input your own images.
+    Demo app for NST (Neural Style Transfer). Select one of the example below or input your own images.
     """,
     inputs=[
         gr.Image(shape=(224, 224), type="pil", label="Content Image"),
